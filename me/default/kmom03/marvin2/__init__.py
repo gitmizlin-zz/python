041e62b13c3231009f528cc7afffe7e37c72f0d1
mizlin <you@example.com>
2015-04-22 15:15:00 +0200

    added kmom05 files

--- me/default/kmom03/marvin2/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-Mi with a simple menu to start up with.
-Mi doesnt do anything, just presents a menu with some choices.
-You should add functinoality to Mi.
-
-"""
